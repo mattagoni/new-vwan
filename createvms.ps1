ebf12a08253ae773d9af500745560f2d2be5815c
mattagoni <46663698+mattagoni@users.noreply.github.com>
2025-04-28 15:16:44 +1000

    Update createvms.ps1

--- createvms.ps1
+++ createvms.ps1
@@ -2,7 +2,7 @@
 New-AzResourceGroupDeployment -ResourceGroupName "magoni" -TemplateFile "/home/matthew/new-vwan/vmcreatetemplate.json" -TemplateParameterFile "/home/matthew/new-vwan/VM1parameters.json"
 New-AzResourceGroupDeployment -ResourceGroupName "magoni" -TemplateFile "/home/matthew/new-vwan/vmcreatetemplate.json" -TemplateParameterFile "/home/matthew/new-vwan/VM2parameters.json"
 
-#az network nic ip-config update --name ipconfig1 --resource-group magoni --nic-name magoni-vm01909 --remove PublicIpAddress
-#Remove-AzureRmPublicIpAddress -Name magoni-vm01-ip -ResourceGroupName magoni -force
-#az network nic ip-config update --name ipconfig1 --resource-group magoni --nic-name vic-vm01910 --remove PublicIpAddress
-#Remove-AzureRmPublicIpAddress -Name magoni-vm02-ip -ResourceGroupName magoni -force
+az network nic ip-config update --name ipconfig1 --resource-group magoni --nic-name magoni-vwan-vm1909 --remove PublicIpAddress
+Remove-AzureRmPublicIpAddress -Name magoni-vwan-vm1-ip -ResourceGroupName magoni -force
+az network nic ip-config update --name ipconfig1 --resource-group magoni --nic-name magoni-vm01910 --remove PublicIpAddress
+Remove-AzureRmPublicIpAddress -Name magoni-vwan-vm2-ip -ResourceGroupName magoni -force
