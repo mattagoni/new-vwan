3c18fb74fe7c394cf1b96f64549023b6db580a2b
mattagoni <46663698+mattagoni@users.noreply.github.com>
2025-04-28 13:42:05 +1000

    Update createvms.ps1

--- createvms.ps1
+++ createvms.ps1
@@ -1,8 +1,8 @@
-New-AzureRmResourceGroup -Name magoni_vms-rg -Location AustraliaSouthEast
-New-AzResourceGroupDeployment -ResourceGroupName "magoni_vms-rg" -TemplateFile "/home/matthew/azurevwan/vmcreatetemplate.json" -TemplateParameterFile "/home/matthew/azurevwan/VM1parameters.json"
-New-AzResourceGroupDeployment -ResourceGroupName "magoni_vms-rg" -TemplateFile "/home/matthew/azurevwan/vmcreatetemplate.json" -TemplateParameterFile "/home/matthew/azurevwan/VM2parameters.json"
+New-AzureRmResourceGroup -Name magoni -Location AustraliaSouthEast
+New-AzResourceGroupDeployment -ResourceGroupName "magoni" -TemplateFile "/home/matthew/azurevwan/vmcreatetemplate.json" -TemplateParameterFile "/home/matthew/azurevwan/VM1parameters.json"
+New-AzResourceGroupDeployment -ResourceGroupName "magoni" -TemplateFile "/home/matthew/azurevwan/vmcreatetemplate.json" -TemplateParameterFile "/home/matthew/azurevwan/VM2parameters.json"
 
-#az network nic ip-config update --name ipconfig1 --resource-group magoni_vms-rg --nic-name magoni-vm01909 --remove PublicIpAddress
-#Remove-AzureRmPublicIpAddress -Name magoni-vm01-ip -ResourceGroupName magoni_vms-rg -force
-#az network nic ip-config update --name ipconfig1 --resource-group magoni_vms-rg --nic-name vic-vm01910 --remove PublicIpAddress
-#Remove-AzureRmPublicIpAddress -Name magoni-vm02-ip -ResourceGroupName magoni_vms-rg -force
+#az network nic ip-config update --name ipconfig1 --resource-group magoni --nic-name magoni-vm01909 --remove PublicIpAddress
+#Remove-AzureRmPublicIpAddress -Name magoni-vm01-ip -ResourceGroupName magoni -force
+#az network nic ip-config update --name ipconfig1 --resource-group magoni --nic-name vic-vm01910 --remove PublicIpAddress
+#Remove-AzureRmPublicIpAddress -Name magoni-vm02-ip -ResourceGroupName magoni -force
