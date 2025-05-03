395cc4b1d49721c5364744d617d875d1a195deb7
mattagoni <46663698+mattagoni@users.noreply.github.com>
2025-05-03 10:28:01 +1000

    Update createvwan.ps1

--- createvwan.ps1
+++ createvwan.ps1
@@ -6,11 +6,4 @@ $vwanName = "magoni_vwan"
 ##createds vwan with 2 Hubs
 #New-AzResourceGroup -Location $location -Name $resourceGroupName
 $virtualWan = New-AzVirtualWan -ResourceGroupName $resourceGroupName -Name $vwanName -Location $location 
-#New-AzVirtualHub -VirtualWan $virtualWan -ResourceGroupName $resourceGroupName -Name "hub1" -AddressPrefix "10.0.1.0/24" -Location $location ; New-AzVirtualHub -VirtualWan $virtualWan -ResourceGroupName $resourceGroupName -Name "hub2" -AddressPrefix "10.0.2.0/24" -Location $location 
-$job1 = Start-Job { New-AzVirtualHub -VirtualWan $using:virtualWan -ResourceGroupName $using:resourceGroupName -Name "hub1" -AddressPrefix "10.0.1.0/24" -Location $using:location }
-$job2 = Start-Job { New-AzVirtualHub -VirtualWan $using:virtualWan -ResourceGroupName $using:resourceGroupName -Name "hub2" -AddressPrefix "10.0.2.0/24" -Location $using:location }
-# Wait for both to finish
-Wait-Job -Job $job1, $job2
-# Check for errors
-Receive-Job -Job $job1
-Receive-Job -Job $job2
+New-AzVirtualHub -VirtualWan $virtualWan -ResourceGroupName $resourceGroupName -Name "hub1" -AddressPrefix "10.0.1.0/24" -Location $location ; New-AzVirtualHub -VirtualWan $virtualWan -ResourceGroupName $resourceGroupName -Name "hub2" -AddressPrefix "10.0.2.0/24" -Location $location 
